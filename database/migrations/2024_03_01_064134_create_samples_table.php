8d5c6bc52a77088ac1e185e36df93d5435782e66
Snow Deep <barondeepsnow@gmail.com>
2024-03-01 14:30:02 +0700

    App S-Marketing

---
+++ database/migrations/2024_03_01_064134_create_samples_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('samples', function (Blueprint $table) {
+            $table->id();
+            $table->string('url')->nullable();
+            $table->integer('type')->nullable();
+            $table->string('name')->nullable();
+            $table->string('age')->nullable();
+            $table->string('sex')->nullable();
+            $table->string('job')->nullable();
+            $table->string('note')->nullable();
+            $table->string('theme')->nullable();
+            $table->string('area')->nullable();
+            $table->string('rate')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('samples');
+    }
+};
