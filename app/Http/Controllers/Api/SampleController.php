c61e834447809dc63bdb245ed430ce51eafa2ff5
Snow Deep <barondeepsnow@gmail.com>
2024-03-15 15:34:34 +0700

    add api collect

--- app/Http/Controllers/Api/SampleController.php
+++ app/Http/Controllers/Api/SampleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Sample;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class SampleController extends Controller
 {
@@ -13,6 +14,7 @@ class SampleController extends Controller
         return response()->json($data,200);
     }
     public function collectData(Request $request){
+        Log::info('Data from contact form 7: '.$request);
         return response()->json($request,200);
     }
 }
