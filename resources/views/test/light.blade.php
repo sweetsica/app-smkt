b4b9acbf7b53b775e50be96e07227672b00953f0
Snow Deep <barondeepsnow@gmail.com>
2024-03-22 17:12:57 +0700

    add test on/off

--- resources/views/test/light.blade.php
+++ resources/views/test/light.blade.php
@@ -1,9 +1,12 @@
 @extends('template.master')
 @section('page-content')
-
-    @if($data['status'] == 1)
-        <img src="{{ asset('admin/assets/images/on.jpg') }}">
-    @else
-        <img src="{{ asset('admin/assets/images/off.jpg') }}">
+    @if($data)
+        @if($data['status'] == 1)
+            <img src="{{ asset('admin/assets/images/on.jpg') }}">
+        @else
+            <img src="{{ asset('admin/assets/images/off.jpg') }}">
+        @endif
     @endif
+    <img src="{{ asset('admin/assets/images/off.jpg') }}">
+
 @endsection
